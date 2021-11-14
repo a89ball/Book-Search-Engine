155ebe6d15f5da1d2cbd51ae8223f68a06d1e07c
Abdifatah <abdifatah.101@outlook.com>
2021-11-14 23:43:59 +0000

    Added google API

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,9 +3,14 @@ const userRoutes = require('./user-routes');
 const path = require("path");
 const bookRoutes = require("./bookRoutes.js");
 const userRoutes= require("./user-routes");
+const googleRoutes= require("./googleRoutes")
 
 
-router.use("/books", )
+router.use("/google", googleRoutes)
+router.use("/books", bookRoutes)
 router.use('/users', userRoutes);
 
+router.use(function (req, res){
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"))
+});
 module.exports = router;
