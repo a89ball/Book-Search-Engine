4f9d2b43d1864ec79e62785c5cbeab09f7ba81a4
Abdifatah <abdifatah.101@outlook.com>
2021-11-14 22:50:23 +0000

    Created book routes & edited index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-
+const path = require("path");
+const bookRoutes = require("./bookRoutes.js")
+const userRoutes= require("./user-routes")
 router.use('/users', userRoutes);
 
 module.exports = router;
