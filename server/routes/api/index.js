6c34e41853f4e11520ad0a4965e566d323827f1d
Abdifatah <abdifatah.101@outlook.com>
2021-11-14 23:20:02 +0000

    set up secret

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const path = require("path");
-const bookRoutes = require("./bookRoutes.js")
-const userRoutes= require("./user-routes")
+const bookRoutes = require("./bookRoutes.js");
+const userRoutes= require("./user-routes");
+
+
+router.use("/books", )
 router.use('/users', userRoutes);
 
 module.exports = router;
