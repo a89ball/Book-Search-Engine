9171a103cb8860f5b54e6a8fd81ebf9b45a43412
Abdifatah <abdifatah.101@outlook.com>
2021-11-15 00:36:07 +0000

    Added graph QL

--- server/server.js
+++ server/server.js
@@ -9,25 +9,44 @@ const { authMiddleware } = require('./utils/auth');
 
 
 require("dotenv").config();
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const PORT = process.env.PORT || 3001;
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start();
+  server.applyMiddleware({ app })
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "client/build")));
+  }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
+
+  mongoose.connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
 }
 
-app.use(routes);
-
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer(typeDefs, resolvers)
